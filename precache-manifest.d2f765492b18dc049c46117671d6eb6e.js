0d1bc0e451046d239295865dca412641f9657635
Diego León Acevedo Arenas <diego.acevedo@proveedores.lineadirecta.com.co>
2023-02-11 19:50:45 -0500

    Updates

---
+++ precache-manifest.d2f765492b18dc049c46117671d6eb6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8f1f9f0509e1670ace1",
+    "url": "/TodoApp.FrontEnd.ReactJs/static/css/main.683affcc.chunk.css"
+  },
+  {
+    "revision": "c8f1f9f0509e1670ace1",
+    "url": "/TodoApp.FrontEnd.ReactJs/static/js/main.c8f1f9f0.chunk.js"
+  },
+  {
+    "revision": "64c4858c1bda8b5cc9be",
+    "url": "/TodoApp.FrontEnd.ReactJs/static/js/1.64c4858c.chunk.js"
+  },
+  {
+    "revision": "f2ada253170365c1f3bc",
+    "url": "/TodoApp.FrontEnd.ReactJs/static/js/runtime~main.f2ada253.js"
+  },
+  {
+    "revision": "e5328f621b51e630b0d658e5739ad5fd",
+    "url": "/TodoApp.FrontEnd.ReactJs/index.html"
+  }
+];
